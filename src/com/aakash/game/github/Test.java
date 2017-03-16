b114a4faa7261c3986d74e95e08f2a30f8fce181
Aakash Manandhar <aakashmanandhar@gmail.com>
2017-03-16 17:35:19 +0545

    Some changes

---
+++ src/com/aakash/game/github/Test.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aakash.game.github;
+
+/**
+ *
+ * @author aakashm
+ */
+public class Test {
+    
+    public static void main (String[] args){
+    int a = 1;
+    int b = 2;
+    
+        System.out.println("The sum of a and b is " + (a + b));
+    
+    }
+       
+}
